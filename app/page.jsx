dc77d00f53bdf798a3901a036a6d1f80fbb222db
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-02-15 02:23:14 +0100

    created the landing page

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,7 @@ export default function Home() {
             </p>
             <Button>Create Quiz</Button>
           </div>
-          <Image src='/images/hero.jpg' width={300} height={300} alt="Cartoon reading book" />
+          <Image src='/Images/hero.jpg' width={300} height={300} alt="Cartoon reading book" />
         </section>
       </div>
       <section className={styles.steps}>
