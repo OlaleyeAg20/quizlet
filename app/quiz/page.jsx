9f5aa9336e168fb2b8385dd07169bb3fa85a0edf
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-02-17 15:33:10 +0100

    implemented opengraph protocol for social media sharing

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css"
 
 function page() {
     const [loggedInUser, setLoggedInUser] = useState({})
@@ -31,7 +33,15 @@ function page() {
 
   return loading ? <h1>Loading...</h1> : (
     <div>
-      <h1>Welcome {loggedInUser.username}</h1>
+      {/* <h1>Welcome {loggedInUser.username}</h1> */}
+      <section className={styles.userSection}>
+        <div className={styles.user}>
+          <Image src='/Images/user.png' width={30} height={30} alt="user image placeholder" />
+          <span>Welcome, {loggedInUser.username.toUpperCase()} 👋</span>
+        </div>
+        <p><span className={styles.label}>Username:</span> {loggedInUser.username}</p>
+        <p><span className={styles.label}>Email:</span> {loggedInUser.email}</p>
+      </section>
     </div>
   );
 }
